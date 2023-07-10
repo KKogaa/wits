080acd517bcc6698b032c39add0a1dfcbd1d7e4b
Kodres <a20160037@pucp.edu.pe>
2023-07-10 14:38:10 -0500

    feat: added collection visualization

--- frontend/src/components/Collection.jsx
+++ frontend/src/components/Collection.jsx
@@ -1,23 +1,35 @@
 import { useNavigate } from "react-router-dom"
 import { Button } from "./Button"
 import { Card } from "./Card"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 export const Collection = () => {
 
+  const [data, setData] = useState([])
   const navigate = useNavigate()
 
-  const fakeData = [
-    {
-      url: "link",
-      source: "source",
-      title: "title",
-    },
-    {
-      url: "link",
-      source: "source",
-      title: "title",
-    },
-  ]
+  useEffect(() => {
+    const fetchData = async () => {
+      // Simulate API call to fetch data
+      try {
+
+        const url = "http://localhost:8080/vectors"
+        const res = await axios.get(`${url}`).then(
+          (response) => {
+            setData(response.data)
+          }
+        ).catch((error) => {
+          console.log(error)
+        })
+
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      }
+    }
+
+    fetchData()
+  }, [])
 
   const uploadImage = async () => {
     navigate("/upload")
@@ -29,8 +41,8 @@ export const Collection = () => {
         <button onClick={uploadImage} className="bg-indigo-400 rounded p-1">Upload</button>
       </div>
       <div className="flex flex-row flex-wrap justify-center">
-        {fakeData.map((item) => (
-          <Card />
+        {data.map((item) => (
+          <Card path={item.path} />
         ))}
       </div>
 
